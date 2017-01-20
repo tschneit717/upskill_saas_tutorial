463846505cca6768f9c1210d7ae4c7c5dd286990
Tom Schneider <tschneit717@gmail.com>
2017-01-20 20:37:42 +0000

    Added email validation for contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ActiveRecord::Base
   # Contact form validations
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, inclusion: { in: %w(@),
+    message: " is not a valid address" }
   validates :comments, presence: true
 end
