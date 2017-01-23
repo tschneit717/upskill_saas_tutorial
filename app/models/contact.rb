34ea7332a0065a633931e0c053d4cbee5a0a3b19
Tom Schneider <tschneit717@gmail.com>
2017-01-23 23:34:15 +0000

    Added conditional logic on sign up form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,6 @@
 class Contact < ActiveRecord::Base
   # Contact form validations
   validates :name, presence: true
-  validates :email, presence: true, inclusion: { in: %w(@),
-    message: " is not a valid address" }
+  validates :email, presence: true
   validates :comments, presence: true
 end
